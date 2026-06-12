309eabd4e198956c4fef502e3f47d253660066dc
ABINAYA R <abinaya.r090903@gmail.com>
2026-06-12 13:08:56 +0530

    Add files via upload

---
+++ products-1.js
@@ -0,0 +1,86 @@
+const products = [
+{
+id:1,
+name:"Pastel Notebook",
+price:"₹199",
+category:"Notebook",
+image:"notebook.jpeg"
+},
+{
+id:2,
+name:"Kawaii Pen Set",
+price:"₹149",
+category:"Pen",
+image:"pens.jpeg"
+},
+{
+id:3,
+name:"Cute Sticky Notes",
+price:"₹99",
+category:"Accessories",
+image:"sticky-notes.jpeg"
+},
+{
+id:4,
+name:"Pastel Pencil Pouch",
+price:"₹249",
+category:"Accessories",
+image:"pouch.jpeg"
+},
+{
+id:5,
+name:"Planner Journal",
+price:"₹299",
+category:"Notebook",
+image:"planner.jpeg"
+},
+{
+id:6,
+name:"Washi Tape Set",
+price:"₹129",
+category:"Accessories",
+image:"washi-tape.jpeg"
+},
+{
+id:7,
+name:"Cute Highlighter Set",
+price:"₹179",
+category:"Pen",
+image:"highlighters.jpeg"
+},
+{
+id:8,
+name:"Aesthetic Bookmark Set",
+price:"₹89",
+category:"Desk Essentials",
+image:"bookmarks.jpeg"
+},
+{
+id:9,
+name:"Pastel Gel Pens",
+price:"₹199",
+category:"Pen",
+image:"gel-pens.jpeg"
+},
+{
+id:10,
+name:"Mini Desk Calendar",
+price:"₹249",
+category:"Desk Essentials",
+image:"calendar.jpeg"
+},
+{
+id:11,
+name:"Cartoon Eraser Pack",
+price:"₹79",
+category:"Desk Essentials",
+image:"erasers.jpeg"
+},
+{
+id:12,
+name:"Cute Ruler Set",
+price:"₹99",
+category:"Desk Essentials",
+image:"rulers.jpeg"
+}
+];
